703643fa35d415570e23d3af4dc5ec5ba2dd9fed
littledumb <jiamin@gmail.com>
2018-09-26 17:10:04 +0800

    major refactoring

--- index.js
+++ index.js
@@ -1,101 +1,50 @@
-const url = require('url');
+'use strict';
+
 const {send, json} = require('micro');
-const query = require('micro-query');
 const request = require('request-promise');
 const cheerio = require('cheerio');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
 const htmlToText = require('html-to-text');
+const detect = require('detect-character-encoding');
 const iconv = require('iconv-lite');
 
-const adapter = new FileSync('sites.json');
-const db = low(adapter);
-const sites = db
-  .defaults({sites: []})
-  .get('sites');
+// Use Pareto's principle to determine the main element
+const pareto = ($, el) => {
+  const total = $(el).text().length;
 
-const addOrReplaceSite = async site => {
-  // Remove duplication if exists
-  await sites.remove({host: site.host}).write();
-
-  const newSite = {
-    host: site.host,
-    selector: site.selector,
-    context: site.context
-  };
-
-  if (site.encoding) {
-    newSite.encoding = site.encoding;
-  }
-
-  return sites.push(newSite).write();
-};
-
-const deleteSite = async site => {
-  return sites.remove({host: site.host}).write();
-};
-
-const read = async (url, site) => {
-  try {
-    let body = await request({
-      uri: url,
-      method: 'GET',
-      encoding: null
-    });
-
-    if (site.encoding && iconv.encodingExists(site.encoding)) {
-      body = iconv.decode(Buffer.from(body), site.encoding);
+  let candidate = el;
+  $(el).children().each((i, child) => {
+    if ($(child).text().length >= 0.8 * total) {
+      candidate = pareto($, child);
     }
+  });
 
-    const $ = cheerio.load(body);
-    const elements = $(site.selector, site.context);
-
-    const text = elements.map((i, el) => {
-      return htmlToText.fromString($(el).html(), {
-        ignoreHref: true
-      });
-    }).get().join('\n\n');
-
-    return text;
-  } catch (err) {
-    console.log(err);
-    return null;
-  }
+  return candidate;
 };
 
 module.exports = async (req, res) => {
-  const params = query(req);
+  try {
+    const params = await json(req);
+    if (!params.url) {
+      return send(res, 400, 'url is expected.\n');
+    }
 
-  let site = (req.method === 'POST' || req.method === 'DELETE') ?
-    await json(req) : null;
-  let host = '';
+    const body = await request({
+      uri: params.url,
+      method: 'GET',
+      encoding: null // Must set to null, otherwise request will use its default encoding
+    });
 
-  switch (req.method) {
-    case 'GET':
-      if (!params || !params.url) {
-        return send(res, 400);
-      }
-      // Check if the site is supported
-      host = url.parse(params.url).hostname;
-      site = await sites.find({host}).value();
-      if (!site) {
-        const hosts = await sites.value().map(s => s.host);
-        return send(res, 404,
-          'We only support the following sites: ' +
-          hosts.join(', '));
-      }
-      return send(res, 200, await read(params.url, site));
-    case 'POST':
-      if (!site || !site.host || !site.selector || !site.context) {
-        return send(res, 400);
-      }
-      return send(res, 200, await addOrReplaceSite(site));
-    case 'DELETE':
-      if (!site || !site.host) {
-        return send(res, 400);
-      }
-      return send(res, 200, await deleteSite(site));
-    default:
-      return send(res, 400);
+    const {encoding, confidence} = detect(body);
+    const $ = iconv.encodingExists(encoding) && confidence > 50 ?
+      cheerio.load(iconv.decode(body, encoding)) :
+      cheerio.load(await request(params.url));
+    const text = htmlToText.fromString($(pareto($, $('body'))).html(), {
+      ignoreHref: true,
+      singleNewLineParagraphs: true
+    }).replace(/\n\s*\n/g, '\n').replace(/\n/g, '\n\n');
+
+    return send(res, 200, text);
+  } catch (error) {
+    return send(res, 400, error.message + '\n');
   }
 };
