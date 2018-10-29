07a4da29091cbcaa5b72c31bf44dd0627cd61058
littledumb <jiamin@gmail.com>
2018-10-29 17:46:15 +0800

    refactoring the code

--- index.js
+++ index.js
@@ -6,14 +6,73 @@ const cheerio = require('cheerio');
 const htmlToText = require('html-to-text');
 const detect = require('chardet');
 const iconv = require('iconv-lite');
+const httpError = require('http-errors');
+const logzio = require('logzio-nodejs');
+const microLogzio = require('micro-logzio');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
+const defaultParetoRatio = 0.6;
+const headerNameForRequest = 'x-request-id';
+const headerNameForCorrelation = 'x-correlation-id';
+const logger = logzio.createLogger({
+  token: process.env.LOGZIO_TOKEN,
+  host: 'listener.logz.io'
+});
+const reqLogger = microLogzio({
+  logger,
+  headerNameForRequest,
+  headerNameForCorrelation
+});
+
+const createContext = (req, res) => {
+  const correlationId = res.getHeader(headerNameForCorrelation);
+  return {correlationId};
+};
+
+const check = params => {
+  const {url} = params;
+  const keepHref = params.keepHref || false;
+  if (!url) {
+    throw httpError(400, 'url is a required parameter.');
+  }
+  const paretoRatio = parseFloat(params.paretoRatio) || defaultParetoRatio;
+  if (paretoRatio >= 1.0 || paretoRatio <= 0.5) {
+    throw httpError(400, 'paretoRatio should be a number between 0.5 and 1.0');
+  }
+  return {url, keepHref, paretoRatio};
+};
+
+const getContent = async (cxt, url) => {
+  const headers = {};
+  headers[headerNameForCorrelation] = cxt.correlationId;
+  const options = {
+    uri: url,
+    method: 'GET',
+    headers,
+    encoding: null // Must set to null, otherwise request will use its default encoding
+  };
+  const content = await request(options);
+  return content;
+};
+
+const getEncoding = content => {
+  const encoding = detect.detectAll(content);
+  if (encoding[0].confidence < 50 || !iconv.encodingExists(encoding[0].name)) {
+    throw httpError(501, 'Can\'t recognize encoding of the web page.');
+  }
+  return encoding[0].name;
+};
 
 // Use Pareto's principle to determine the main element
-const pareto = ($, el) => {
+const pareto = ($, el, p) => {
   const total = $(el).text().length;
 
   let candidate = el;
   $(el).children().each((i, child) => {
-    if ($(child).text().length >= 0.8 * total) {
+    if ($(child).text().length >= p * total) {
       candidate = pareto($, child);
     }
   });
@@ -21,33 +80,24 @@ const pareto = ($, el) => {
   return candidate;
 };
 
-module.exports = async (req, res) => {
+module.exports = reqLogger(async (req, res) => {
   try {
-    const params = await json(req);
-    if (!params.url) {
-      return send(res, 400, 'url is expected.\n');
-    }
+    const cxt = createContext(req, res);
+    const {url, keepHref, paretoRatio} = check(await json(req));
 
-    const body = await request({
-      uri: params.url,
-      method: 'GET',
-      encoding: null // Must set to null, otherwise request will use its default encoding
-    });
+    const content = await getContent(cxt, url);
+    const encoding = getEncoding(content);
 
-    const encoding = detect.detectAll(body);
-    if (encoding[0].confidence < 50 || !iconv.encodingExists(encoding[0].name)) {
-      return send(res, 400, 'an encoding issue is encountered.\n');
-    }
-    const $ = cheerio.load(iconv.decode(body, encoding[0].name));
-    const html = $(pareto($, $('body'))).html();
-    const ignoreHref = !params.keepHref;
+    const $ = cheerio.load(iconv.decode(content, encoding));
+    const html = $(pareto($, $('body'), paretoRatio)).html();
     const text = htmlToText.fromString(html, {
-      ignoreHref,
+      ignoreHref: !keepHref,
       singleNewLineParagraphs: true
     }).replace(/\n\s*\n/g, '\n').replace(/\n/g, '\n\n');
 
     return send(res, 200, text);
   } catch (error) {
-    return send(res, 400, error.message + '\n');
+    return send(res, error.statusCode || 500, error.message || 'Something wrong with the service.');
   }
-};
+});
+
