5255c08d92dff4ac4b58bf2e7ea668e1eb20d514
littledumb <jiamin@gmail.com>
2018-12-01 23:39:39 +0800

    allow compressed data, add test

--- index.js
+++ index.js
@@ -27,22 +27,23 @@ const check = params => {
   return {url, keepHref, paretoRatio, encoding};
 };
 
-const getContent = async url => {
-  const options = {
-    uri: url,
-    method: 'GET',
-    encoding: null // Must set to null, otherwise request will use its default encoding
-  };
-  const content = await request(options);
-  return content;
-};
-
 const getEncoding = content => {
   const encoding = detect.detectAll(content);
   if (encoding[0].confidence < 50 || !iconv.encodingExists(encoding[0].name)) {
     throw httpError(501, 'Can\'t recognize encoding of the web page.');
   }
   return encoding[0].name;
+};
+
+const getContent = async (url, encoding) => {
+  const options = {
+    uri: url,
+    method: 'GET',
+    gzip: true,
+    encoding: null // Must set to null, otherwise request will use its default encoding
+  };
+  const content = await request(options);
+  return iconv.decode(content, encoding || getEncoding(content));
 };
 
 // Use Pareto's principle to determine the main element
@@ -63,9 +64,8 @@ module.exports = async (req, res) => {
   try {
     const {url, keepHref, paretoRatio, encoding} = check(await json(req));
 
-    const content = await getContent(url);
-
-    const $ = cheerio.load(iconv.decode(content, encoding || getEncoding(content)));
+    const content = await getContent(url, encoding);
+    const $ = cheerio.load(content);
     const html = $(pareto($, $('body'), paretoRatio)).html();
     const text = htmlToText.fromString(html, {
       ignoreHref: !keepHref,
---
+++ test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const micro = require('micro');
+const test = require('ava');
+const listen = require('test-listen');
+const request = require('request-promise');
+
+const service = require('.');
+
+let s = null;
+let u = null;
+
+const testRead = async (t, url) => {
+  try {
+    await request.post(u).json({url});
+    t.pass();
+  } catch (error) {
+    t.fail();
+  }
+};
+
+test.before(async t => {
+  s = micro(service);
+  u = await listen(s);
+  t.pass();
+});
+
+test.after('close service', t => {
+  s.close();
+  t.pass();
+});
+
+test('gbk', async t => {
+  const url = 'http://www.biquge.com.tw/3_3410/1939763.html';
+  await testRead(t, url);
+});
+
+test('utf8, compressed', async t => {
+  const url = 'https://www.80txt.com/txtml_40079.html';
+  await testRead(t, url);
+});
+
+test('gb2312, compressed', async t => {
+  const url = 'http://www.jjwxc.net/onebook.php?novelid=1469663';
+  await testRead(t, url);
+});
